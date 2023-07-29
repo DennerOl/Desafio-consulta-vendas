580349aaa5e2dc5afb1903ae84f78d703894656a
DennerOl <dener155@hotmail.com>
2023-07-29 18:24:22 -0300

    Repositorio

--- src/main/java/com/devsuperior/dsmeta/controllers/SaleController.java
+++ src/main/java/com/devsuperior/dsmeta/controllers/SaleController.java
@@ -1,6 +1,9 @@
 package com.devsuperior.dsmeta.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,21 +19,20 @@ public class SaleController {
 
 	@Autowired
 	private SaleService service;
-	
+
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<SaleMinDTO> findById(@PathVariable Long id) {
 		SaleMinDTO dto = service.findById(id);
 		return ResponseEntity.ok(dto);
 	}
-
 	@GetMapping(value = "/report")
-	public ResponseEntity<?> getReport() {
+	public ResponseEntity<Page<SaleReportDTO>> getReport(
 		// TODO
 		return null;
 	}
 
 	@GetMapping(value = "/summary")
-	public ResponseEntity<?> getSummary() {
+	public ResponseEntity<List<SaleSummaryDTO>> getSummary(
 		// TODO
 		return null;
 	}
--- src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
+++ src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
@@ -1,9 +1,39 @@
 package com.devsuperior.dsmeta.repositories;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.time.LocalDate;
+import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import com.devsuperior.dsmeta.entities.Sale;
+
 
 public interface SaleRepository extends JpaRepository<Sale, Long> {
 
+	
+    @Query(nativeQuery = true,
+            value = "SELECT tb_sales.id, tb_sales.date, tb_sales.amount, tb_seller.name "
+                    + "FROM tb_sales "
+                    + "INNER JOIN tb_seller ON tb_seller.id = tb_sales.seller_id "
+                    + "WHERE UPPER(tb_seller.name) LIKE CONCAT('%',UPPER(:name),'%') AND "
+                    + "tb_sales.date BETWEEN :minDate AND :maxDate",
+                    countQuery = "SELECT COUNT(*) FROM tb_sales INNER JOIN tb_seller ON tb_seller.id = tb_sales.seller_id "
+                            + "WHERE UPPER(tb_seller.name) LIKE CONCAT('%',UPPER(:name),'%') AND "
+                            + "tb_sales.date BETWEEN :minDate AND :maxDate")
+    Page<ReportProjection> findReport(LocalDate minDate, LocalDate maxDate, String name, Pageable pageable);
+
+    
+    
+    @Query(nativeQuery = true,
+            value = "SELECT tb_seller.name, SUM(tb_sales.amount) AS salesAmount "
+                    + "FROM tb_sales "
+                    + "INNER JOIN tb_seller ON tb_seller.id = tb_sales.seller_id "
+                    + "WHERE tb_sales.date BETWEEN :minDate AND :maxDate "
+                    + "GROUP BY tb_seller.name")
+    List<SummaryProjection> findSummary(LocalDate minDate, LocalDate maxDate);
+
+
+  
 }
